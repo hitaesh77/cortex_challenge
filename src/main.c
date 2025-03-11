dc773e0189a7b5302d70c25049b9ad083dba89c3
hitaesh77 <hitaesh777@gmail.com>
2025-03-11 01:50:29 -0400

    Used USART to make printf work. Working on Data aquisition and processing

--- src/main.c
+++ src/main.c
@@ -5,27 +5,40 @@
 
 void internal_clock();
 
-/*
-//ENABLE NECESSARY PORTS
-void enable_ports(void) {
-    //enable gpiob and gpioc
-    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+//ENABLE USART FOR PRINTF
+void init_usart5() {
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    RCC->AHBENR |= RCC_AHBENR_GPIODEN;
 
-    GPIOB->MODER &= ~((3 << (0 * 2)) | (3 << (1 * 2)) | (3 << (2 * 2)) | (3 << (3 * 2)) | (3 << (4 * 2) | (3 << (5 * 2)))); // reset pins 0-5
-    GPIOB->MODER &= ~((3 << (6 * 2)) | (3 << (7 * 2)) | (3 << (8 * 2)) | (3 << (9 * 2)) | (3 << (10 * 2))); // reset pins 6-10
+    //PC12
+    GPIOC->MODER &= ~(3 << (12 * 2));
+    GPIOC->MODER |= (2 << (12 * 2)); 
+    GPIOC->AFR[1] |= (2 << (4 * (12 - 8))); //AF2 with PC12
 
-    GPIOB->MODER |= ((1 << (0 * 2)) | (1 << (1 * 2)) | (1 << (2 * 2)) | (1 << (3 * 2)) | (1 << (4 * 2)) | (1 << (5 * 2))); // set pins 0-5 to output
-    GPIOB->MODER |= ((1 << (6 * 2)) | (1 << (7 * 2)) | (1 << (8 * 2)) | (1 << (9 * 2)) | (1 << (10 * 2))); // set pins 6-10 to output
+    //PD2
+    GPIOD->MODER &= ~(3 << (2 * 2));
+    GPIOD->MODER |= (2 << (2 * 2));
+    GPIOD->AFR[0] |= (2 << (4 * 2)); //AF2 with PD2
 
-    GPIOC->MODER &= ~((3 << (4 * 2)) | (3 << (5 * 2)) | (3 << (6 * 2)) | (3 << (7 * 2))); // reset pins 4-7
-    GPIOC->MODER |= ((1 << (4 * 2)) | (1 << (5 * 2)) | (1 << (6 * 2)) | (1 << (7 * 2))); // set pins 4-7 to output
-    GPIOC->OTYPER |= (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7); // set pins 4-7 to open drain
-
-    GPIOC->MODER &= ~((3 << 0) | (3 << (1 * 2)) | (3 << (2 * 2)) | (3 << (3 * 2))); // reset pins 0-3 to make them input
-    GPIOC->PUPDR |= ((1 << 0) | (1 << (1 * 2)) | (1 << (2 * 2)) | (1 << (3 * 2))); // set pins 0-3 as pulled up
+    RCC->APB1ENR |= RCC_APB1ENR_USART5EN;
+    USART5->CR1 &= ~USART_CR1_UE; //disable USART
+    USART5->CR1 &= ~USART_CR1_M; //8 bit data
+    USART5->CR2 &= ~USART_CR2_STOP; //1 stop bit
+    USART5->CR1 &= ~USART_CR1_PCE; //no parity
+    USART5->CR1 &= ~USART_CR1_OVER8; //oversampling by 16
+    USART5->BRR = 48000000 / 115200; //baud rate 115200
+    USART5->CR1 |= USART_CR1_TE; //enable transmitter
+    USART5->CR1 |= USART_CR1_RE; //enable receiver
+    USART5->CR1 |= USART_CR1_UE; //enable USART
 }
-    */
+
+int __io_putchar(int c) {
+    while (!(USART5->ISR & USART_ISR_TXE));  // Wait until the TX buffer is empty
+    USART5->TDR = (c & 0xFF);  // Send character to USART5
+    return c;
+}
+//END USART FOR PRINTF
+
 
 //SETTING UP AMPLITUDE CONTROL WITH ADC + POTENTIOMETER
 uint32_t amplitude = 2048;
@@ -59,9 +72,11 @@ void TIM2_IRQHandler(){
     bcsum -= boxcar[bcn];
     bcsum += boxcar[bcn] = ADC1->DR;
     bcn += 1;
-    if (bcn >= BCSIZE)
+
+    if (bcn >= BCSIZE) {
         bcn = 0;
         amplitude = bcsum / BCSIZE;
+    }
 }
 
 void init_tim2(void) {
@@ -83,6 +98,7 @@ void init_tim2(void) {
 short int wavetable[N];
 int step = (1<<16); //step = 65536 for 20hz wave
 int offset = 0;
+int sample = 0;
 
 void init_wavetable(void) {
     for(int i=0; i < N; i++)
@@ -108,11 +124,11 @@ void TIM6_DAC_IRQHandler() {
         offset -= (N << 16);
     }
 
-    int samp = wavetable[offset>>16];
-    samp *= amplitude;
-    samp >>= 17; 
-    samp += 2048;
-    DAC->DHR12R1 = samp;
+    sample = wavetable[offset>>16];
+    sample *= amplitude;
+    sample >>= 17; 
+    sample += 2048;
+    DAC->DHR12R1 = sample;
 }
 
 void init_tim6(void) {
@@ -130,6 +146,46 @@ void init_tim6(void) {
 }
 //END SIN WAVE AND DAC
 
+
+
+//DATA AQUISTION AND PROCESSING
+#define BUFFER_SIZE 5  //moving average of 5 samples
+int buffer[BUFFER_SIZE]; //circular buffer
+int buffer_index = 0; //buffer index
+int moving_avg = 0; //moving average
+
+int calc_mov_avg(){
+    int sum = 0;
+    for(int i = 0; i < BUFFER_SIZE; i++){
+        sum += buffer[i];
+    }
+    return sum / BUFFER_SIZE;
+}
+
+// Timer 2 interrupt handler
+void TIM1_IRQHandler(void) {
+    TIM1->SR &= ~TIM_SR_UIF;  // Clear interrupt flag
+    buffer[buffer_index] = sample;
+    buffer_index = (buffer_index + 1) % BUFFER_SIZE;
+
+    moving_avg = calc_mov_avg();
+    printf("Moving average: %d\n", moving_avg);
+}
+
+void init_tim1(void) {
+    RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+
+    //interrupt is invoked every 10ms (100Hz)
+    TIM1->PSC = 48 - 1;  //psc = 48
+    TIM1->ARR = 10000 - 1;  //arr = 10000
+
+    // Enable update interrupt for Timer 2
+    TIM1->DIER |= TIM_DIER_UIE;
+    NVIC_EnableIRQ(TIM2_IRQn);
+    TIM1->CR1 |= TIM_CR1_CEN;    
+}
+//END DATA AQUISTION AND PROCESSING
+
 int main(void){
     internal_clock();
     //enable_ports();
@@ -138,6 +194,10 @@ int main(void){
     init_wavetable();
     setup_dac();
     init_tim6();
+    init_tim1();
+    init_usart5();
+
+    printf("Starting...\n");
 
     while(1);
 }
